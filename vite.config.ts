900eaa84e3c58d08349ed7be0941dd11482b4391
vivek kumar verma <vivek.impdoc@gmail.com>
2026-06-09 17:44:23 +0530

    j

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/police-ai-directory---sandbox/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
